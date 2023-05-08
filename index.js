06f1c8136e315f2296abe9553a0ff2818f415370
José Ferreira <jose.ferreira@mindswap.academy>
2023-05-08 16:43:52 +0100

    Using Jwt to set up authentication

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 "use strict";
 
 const Hapi = require("@hapi/hapi");
-const console = require("console");
+const jwt = require("jsonwebtoken");
+const HapiJWT = require("hapi-auth-jwt");
 
 const init = async () => {
   const server = Hapi.server({
     port: 3000,
     host: "localhost",
   });
+
+  await server.register(HapiJWT);
+
+  server.auth.strategy("jwt", "jwt", {
+    key: "my-key",
+    validate: async (decoded, request, h) => {
+      return { isValid: true };
+    },
+  });
+
+  server.auth.default("jwt");
 
   await server.start();
   console.log("Server started", server.info.uri);
